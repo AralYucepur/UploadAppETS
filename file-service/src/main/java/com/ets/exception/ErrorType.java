e8eb71fb744c298b8f0ef371c73e9ef237e70d65
aral9883 <aral_458@hotmail.com>
2024-03-24 23:58:42 +0300

    Upload Error is added.

--- file-service/src/main/java/com/ets/exception/ErrorType.java
+++ file-service/src/main/java/com/ets/exception/ErrorType.java
@@ -11,7 +11,7 @@ import org.springframework.http.HttpStatus;
 public enum ErrorType{
 
     INTERNAL_ERROR(5100,"Sunucuda beklenmeyen hata oluştu.",HttpStatus.INTERNAL_SERVER_ERROR),
-    REGISTER_USED_USERNAME_ERROR(4110,"Kullanıcı adı başkası tarafından kullanılıyor.",HttpStatus.BAD_REQUEST),
+    REGISTER_USED_USERNAME_ERROR(4110,"Kullanıcı adı kullanılıyor.",HttpStatus.BAD_REQUEST),
     REGISTER_UNMATCHED_REPASSWORD_ERROR(4111,"Şifre ve şifre tekrarı aynı olmalı.",HttpStatus.BAD_REQUEST),
     LOGIN_ERROR(4112,"Kullanıcı adı veya şifre hatalı.",HttpStatus.BAD_REQUEST),
     JWT_TOKEN_CREATE_ERROR(4113,"Token oluşturulamadı.",HttpStatus.BAD_REQUEST),
@@ -20,7 +20,8 @@ public enum ErrorType{
     INVALID_AUTHORITY(4115,"Yetkisiz Kullanıcı.",HttpStatus.BAD_REQUEST),
     INACTIVE_ACCOUNT(4116,"Hesabınız aktif değil. Lütfen Hesabınızı mail adresine gelen kodu kullanarak aktifleştiriniz",HttpStatus.BAD_REQUEST),
     BLOCKED_ACCOUNT(4117,"Hesabınız yasaklanmıştır.",HttpStatus.BAD_REQUEST),
-    WRONG_ACTIVATIONCODE(4118,"Yanlış aktivasyon kodu.",HttpStatus.BAD_REQUEST)
+    WRONG_ACTIVATIONCODE(4118,"Yanlış aktivasyon kodu.",HttpStatus.BAD_REQUEST),
+    UPLOAD_ERROR(4210,"Dosya yüklenirken hata oluştu.",HttpStatus.BAD_REQUEST),
     ;
 
 
